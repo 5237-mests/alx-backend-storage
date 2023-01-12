05608edf14a6aab93987ae31d6d94da4de4cd1af
5237-mests <msfnmulgeta@gmail.com>
2023-01-12 21:37:37 +0300

    avrg

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,4 +1,5 @@
--- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student. Note: An average score can be a decimal.
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes 
+-- and store the average score for a student. Note: An average score can be a decimal.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER |
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
