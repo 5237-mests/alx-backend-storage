a19a71ac0716bf254ae3875435fb307e2a1c2bdf
5237-mests <msfnmulgeta@gmail.com>
2023-01-16 23:17:02 +0300

    changed

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
-"""Module for task 12
-"""
+'''Task 12's module.
+'''
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def print_nginx_request_logs(nginx_collection):
+    '''Prints stats about Nginx request logs.
+    '''
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
+
+
+def run():
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    print_nginx_request_logs(client.logs.nginx)
 
-    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    def log_stats(my_collection):
-        """prints all stats"""
-        print('{} logs:'.format(len(list(my_collection.find()))))
-
-        for methodn in method:
-            numl = list(my_collection.find({'method': methodn}))
-            print('\tmethod {}: {}'.format(methodn, len(numl)))
-
-        stat = list(my_collection.find({'method': 'GET', 'path': '/status'}))
-        print('{} status check'.format(len(stat)))
-
-    log_stats(nginx_collection)
+if __name__ == '__main__':
+    run()
